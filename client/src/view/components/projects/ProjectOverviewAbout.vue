51c5fe625caee6eef131bd6b1099a89c5059e1c4
shcherbak <shcherbak.ssa@gmail.com>
2023-02-06 10:26:30 +0100

    refactor: remove dates from project overview

--- client/src/view/components/projects/ProjectOverviewAbout.vue
+++ client/src/view/components/projects/ProjectOverviewAbout.vue
@@ -12,26 +12,6 @@
 
         <p v-else class="no-description py-1">No description</p>
       </div>
-
-      <div class="flex items-center gap-4">
-        <div class="flex items-center gap-2">
-          <BaseLabel>Created</BaseLabel>
-          <BaseDatePicker
-            placeholder="Created"
-            :value="props.project.createdAt"
-            :readonly="true"
-          />
-        </div>
-
-        <div class="flex items-center gap-2">
-          <BaseLabel class="shrink-0">Due Date</BaseLabel>
-          <BaseDatePicker
-            placeholder="Not due date"
-            :value="props.project.dueDate"
-            :readonly="true"
-          />
-        </div>
-      </div>
     </div>
   </div>
 </template>
@@ -40,7 +20,6 @@
 import type { Project } from 'models/project';
 import type { ViewTab } from 'shared/types';
 
-import BaseDatePicker from 'view/components/base/BaseDatePicker.vue';
 import ContentSection from 'view/components/ContentSection.vue';
 
 type Props = {
@@ -50,12 +29,10 @@ type Props = {
 // Properties
 const props = defineProps<Props>();
 
-const sectionAboutTabs: ViewTab[] = [
-  {
-    label: 'About',
-    key: 'about',
-  },
-];
+const sectionAboutTabs: ViewTab[] = [{
+  label: 'About',
+  key: 'about',
+}];
 </script>
 
 <style scoped lang="scss">
